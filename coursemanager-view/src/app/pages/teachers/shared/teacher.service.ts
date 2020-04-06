c648e20ff359eae7ae6076765f4fa24923af96e6
Gustavo Avila <gustavogavila01@gmail.com>
2020-04-05 22:02:39 -0300

    Listagem de professores

---
+++ coursemanager-view/src/app/pages/teachers/shared/teacher.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+import { Observable, throwError } from "rxjs";
+import { map, catchError, flatMap } from "rxjs/operators";
+import { Teacher } from './teacher.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeacherService {
+
+  private apiPath: string = "http://localhost:8080/teachers"
+
+  constructor(private http: HttpClient) { }
+
+  findAll(): Observable<Teacher[]> {
+    return this.http.get(this.apiPath).pipe(
+      map(this.jsonDataToTeachers),
+      catchError(this.handleError)
+    )
+  }
+
+  findById(id: number): Observable<Teacher> {
+    const url = `${this.apiPath}/${id}`;
+    return this.http.get(url).pipe(
+      map(this.jsonDataToTeacher),
+      catchError(this.handleError)
+    )
+  }
+
+  save(teacher: Teacher): Observable<Teacher> {
+    return this.http.post(this.apiPath, teacher).pipe(
+      map(this.jsonDataToTeacher),
+      catchError(this.handleError)
+    )
+  }
+
+  update(teacher: Teacher): Observable<Teacher> {
+    const url = `${this.apiPath}/${teacher.id}`;
+    return this.http.put(url, teacher).pipe(
+      map(this.jsonDataToTeacher),
+      catchError(this.handleError)
+    )
+  }
+
+  deleteById(id: number): Observable<any> {
+    const url = `${this.apiPath}/${id}`;
+    return this.http.delete(url).pipe(
+      map(() => null),
+      catchError(this.handleError)
+    )
+  }
+
+  private jsonDataToTeachers(jsonData: any[]): Teacher[] {
+    const teachers: Teacher[] = [];
+    jsonData.forEach(element => teachers.push(element as Teacher))
+    return teachers;
+  }
+
+  private jsonDataToTeacher(jsonData: any): Teacher {
+    return jsonData as Teacher;
+  }
+
+  private handleError(error: any): Observable<any> {
+    console.log("Erro na requisição: ", error);
+    return throwError(error);
+  }
+}
--- coursemanager-view/src/app/pages/teachers/teacher-list/teacher-list.component.ts
+++ coursemanager-view/src/app/pages/teachers/teacher-list/teacher-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TeacherService } from '../shared/teacher.service';
+import { Teacher } from '../shared/teacher.model';
+
 @Component({
   selector: 'app-teacher-list',
   templateUrl: './teacher-list.component.html',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeacherListComponent implements OnInit {
 
-  constructor() { }
+  teachers: Teacher[] = [];
+
+  constructor(private teacherService: TeacherService) { }
 
   ngOnInit(): void {
+    this.teacherService.findAll().subscribe(
+      teachers => this.teachers = teachers,
+      () => alert('Erro ao carregar a lista'),
+    );
   }
+
+  deleteTeacher(id: number) {
+    const confirmDelete = confirm('Deseja realmente excluir este item?');
+
+    if (confirmDelete) {
+      this.teacherService.deleteById(id).subscribe(
+        () => this.teachers = this.teachers.filter(t => t.id != id),
+        () => alert('Erro ao remover registro'),
+        );
+    }
+  }
+
 
 }
