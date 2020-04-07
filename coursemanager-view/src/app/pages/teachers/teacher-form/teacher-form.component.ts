95500e3c417f541ef4998450d7d7331120af52ce
Gustavo Avila <gustavogavila01@gmail.com>
2020-04-07 15:57:36 -0300

    Criação da BaseResourceFormComponent e refatoração do form de professores

--- coursemanager-view/src/app/pages/teachers/teacher-form/teacher-form.component.ts
+++ coursemanager-view/src/app/pages/teachers/teacher-form/teacher-form.component.ts
@@ -1,105 +1,24 @@
-import { AfterContentChecked, Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-
-import { switchMap } from 'rxjs/operators';
+import { Component, Injector } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { BaseResourceFormComponent } from 'src/app/shared/components/base-resource-form.component';
 import { Teacher } from '../shared/teacher.model';
 import { TeacherService } from '../shared/teacher.service';
-import toastr from 'toastr';
 
 @Component({
   selector: 'app-teacher-form',
   templateUrl: './teacher-form.component.html',
   styleUrls: ['./teacher-form.component.css'],
 })
-export class TeacherFormComponent implements OnInit, AfterContentChecked {
-  currentAction: string;
-  teacherForm: FormGroup;
-  pageTitle: string;
-  serverErrorMessages: string[];
-  submittingForm: boolean = false;
-  teacher: Teacher = new Teacher();
-
+export class TeacherFormComponent extends BaseResourceFormComponent<Teacher> {
   constructor(
-    private teacherService: TeacherService,
-    private route: ActivatedRoute,
-    private router: Router,
-    private formBuilder: FormBuilder
-  ) {}
-
-  ngOnInit(): void {
-    this.setCurrentAction();
-    this.buildTeacherForm();
-    this.loadTeacher();
+    protected injector: Injector,
+    protected teacherService: TeacherService
+  ) {
+    super(injector, new Teacher(), teacherService, Teacher.fromJson);
   }
 
-  ngAfterContentChecked(): void {
-    this.setPageTitle();
-  }
-
-  submitForm() {
-    this.submittingForm = true;
-    if (this.currentAction === 'new') {
-      this.saveTeacher();
-    } else {
-      this.updateTeacher();
-    }
-  }
-
-  // Private Methods
-
-  private saveTeacher() {
-    const teacher: Teacher = Object.assign(
-      new Teacher(),
-      this.teacherForm.value
-    );
-
-    this.teacherService.save(teacher).subscribe(
-      (teacher) => this.actionsForSuccess(teacher),
-      (error) => this.actionsForError(error)
-    );
-  }
-
-  private updateTeacher() {
-    const teacher: Teacher = Object.assign(
-      new Teacher(),
-      this.teacherForm.value
-    );
-
-    this.teacherService.update(teacher).subscribe(
-      (teacher) => this.actionsForSuccess(teacher),
-      (error) => this.actionsForError(error)
-    );
-  }
-
-  private actionsForSuccess(teacher: Teacher): void {
-    toastr.success('Dados salvos com sucesso!');
-    this.router
-      .navigateByUrl('teachers', { skipLocationChange: true })
-      .then(() => this.router.navigate(['teachers', teacher.id, 'edit']));
-  }
-
-  private actionsForError(error: any): void {
-    toastr.error('Ocorreu um erro ao processar sua solicitação.');
-
-    this.submittingForm = false;
-
-    if (error.status === 422) {
-      this.serverErrorMessages = JSON.parse(error._body).errors;
-    } else {
-      this.serverErrorMessages = [
-        'Falha na comunicação com o servidor. Tente novamente mais tarde.',
-      ];
-    }
-  }
-
-  private setCurrentAction(): void {
-    this.currentAction =
-      this.route.snapshot.url[0].path === 'new' ? 'new' : 'edit';
-  }
-
-  private buildTeacherForm() {
-    this.teacherForm = this.formBuilder.group({
+  protected buildResourceForm = (): void => {
+    this.resourceForm = this.formBuilder.group({
       id: [null],
       name: [
         null,
@@ -112,29 +31,10 @@ export class TeacherFormComponent implements OnInit, AfterContentChecked {
       email: [null, [Validators.required, Validators.email]],
       phone: [null],
     });
-  }
+  };
 
-  private loadTeacher(): void {
-    if (this.currentAction === 'edit') {
-      this.route.paramMap
-        .pipe(
-          switchMap((params) => this.teacherService.findById(+params.get('id')))
-        )
-        .subscribe(
-          (teacher) => {
-            this.teacher = teacher;
-            this.teacherForm.patchValue(teacher);
-          },
-          () => alert('Ocorreu um erro no servidor, tente mais tarde.')
-        );
-    }
-  }
+  protected creationPageTitle = (): string => 'Cadastro de Novo Professor';
 
-  private setPageTitle() {
-    const teacherName = this.teacher?.name || '';
-    this.pageTitle =
-      this.currentAction === 'new'
-        ? 'Cadastro de novo professor'
-        : `Editando ${teacherName}`;
-  }
+  protected editionPageTitle = (): string =>
+    this.resource?.name ? `Editando Professor: ${this.resource?.name}` : '';
 }
---
+++ coursemanager-view/src/app/shared/components/base-resource-form.component.ts
@@ -0,0 +1,133 @@
+import { AfterContentChecked, Injector, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { switchMap } from 'rxjs/operators';
+import toastr from 'toastr';
+import { BaseResourceModel } from '../models/base-resource.model';
+import { BaseResourceService } from '../services/base-resource.service';
+
+export abstract class BaseResourceFormComponent<T extends BaseResourceModel>
+  implements OnInit, AfterContentChecked {
+  currentAction: string;
+  resourceForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm: boolean = false;
+
+  protected route: ActivatedRoute;
+  protected router: Router;
+  protected formBuilder: FormBuilder;
+
+  constructor(
+    protected injector: Injector,
+    protected resource: T,
+    protected resourceService: BaseResourceService<T>,
+    protected jsonDataToResourceFn: (jsonData: any) => T
+  ) {
+    this.route = this.injector.get(ActivatedRoute);
+    this.router = this.injector.get(Router);
+    this.formBuilder = this.injector.get(FormBuilder);
+  }
+
+  ngOnInit(): void {
+    this.setCurrentAction();
+    this.buildResourceForm();
+    this.loadResource();
+  }
+
+  ngAfterContentChecked(): void {
+    this.setPageTitle();
+  }
+
+  submitForm() {
+    this.submittingForm = true;
+    if (this.currentAction === 'new') {
+      this.saveResource();
+    } else {
+      this.updateResource();
+    }
+  }
+
+  // Protected Methods
+
+  protected setCurrentAction = (): void => {
+    this.currentAction =
+      this.route.snapshot.url[0].path === 'new' ? 'new' : 'edit';
+  };
+
+  protected abstract buildResourceForm(): void;
+
+  protected loadResource = (): void => {
+    if (this.currentAction === 'edit') {
+      this.route.paramMap
+        .pipe(
+          switchMap((params) =>
+            this.resourceService.findById(+params.get('id'))
+          )
+        )
+        .subscribe(
+          (resource) => {
+            this.resource = resource;
+            this.resourceForm.patchValue(resource);
+          },
+          () => alert('Ocorreu um erro no servidor, tente mais tarde.')
+        );
+    }
+  };
+
+  protected setPageTitle() {
+    this.pageTitle =
+      this.currentAction === 'new'
+        ? this.creationPageTitle()
+        : this.editionPageTitle();
+  }
+
+  protected creationPageTitle = (): string => 'Novo';
+
+  protected editionPageTitle = (): string => 'Edição';
+
+  protected saveResource = () => {
+    const resource: T = this.jsonDataToResourceFn(this.resourceForm.value);
+
+    this.resourceService.save(resource).subscribe(
+      (resource) => this.actionsForSuccess(resource),
+      (error) => this.actionsForError(error)
+    );
+  };
+
+  protected updateResource = () => {
+    const resource: T = this.jsonDataToResourceFn(this.resourceForm.value);
+
+    this.resourceService.update(resource).subscribe(
+      (resource) => this.actionsForSuccess(resource),
+      (error) => this.actionsForError(error)
+    );
+  };
+
+  protected actionsForSuccess = (resource: T): void => {
+    toastr.success('Dados salvos com sucesso!');
+
+    const baseComponentPath: string = this.route.snapshot.parent.url[0].path;
+
+    this.router
+      .navigateByUrl(baseComponentPath, { skipLocationChange: true })
+      .then(() =>
+        this.router.navigate([baseComponentPath, resource.id, 'edit'])
+      );
+  };
+
+  protected actionsForError(error: any): void {
+    toastr.error('Ocorreu um erro ao processar sua solicitação.');
+
+    this.submittingForm = false;
+
+    if (error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.serverErrorMessages = [
+        'Falha na comunicação com o servidor. Tente novamente mais tarde.',
+      ];
+    }
+  }
+}
