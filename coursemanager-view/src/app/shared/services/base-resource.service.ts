33843f8d957d424706ab0aa21e4674a245561435
Gustavo Avila <gustavogavila01@gmail.com>
2020-04-13 12:58:35 -0300

    Configurando tslint e prettier para padronização de código

--- coursemanager-view/src/app/shared/services/base-resource.service.ts
+++ coursemanager-view/src/app/shared/services/base-resource.service.ts
@@ -22,27 +22,27 @@ export abstract class BaseResourceService<T extends BaseResourceModel> {
     return this.http
       .get(this.apiPath)
       .pipe(map(this.jsonDataToResources), catchError(this.handleError));
-  };
+  }
 
   findById = (id: number): Observable<T> => {
     const url = `${this.apiPath}/${id}`;
     return this.http
       .get(url)
       .pipe(map(this.jsonDataToResource), catchError(this.handleError));
-  };
+  }
 
   save = (resource: T): Observable<T> => {
     return this.http
       .post(this.apiPath, resource)
       .pipe(map(this.jsonDataToResource), catchError(this.handleError));
-  };
+  }
 
   update = (resource: T): Observable<T> => {
     const url = `${this.apiPath}/${resource.id}`;
     return this.http
       .put(url, resource)
       .pipe(map(this.jsonDataToResource), catchError(this.handleError));
-  };
+  }
 
   deleteById = (id: number): Observable<any> => {
     const url = `${this.apiPath}/${id}`;
@@ -50,16 +50,16 @@ export abstract class BaseResourceService<T extends BaseResourceModel> {
       map(() => null),
       catchError(this.handleError)
     );
-  };
+  }
 
   protected jsonDataToResources = (jsonData: any): T[] =>
-    jsonData.map((element: any) => this.jsonDataToResourceFn(element));
+    jsonData.map((element: any) => this.jsonDataToResourceFn(element))
 
   protected jsonDataToResource = (jsonData: any): T =>
-    this.jsonDataToResourceFn(jsonData);
+    this.jsonDataToResourceFn(jsonData)
 
   protected handleError = (error: any): Observable<any> => {
     console.log('Erro na requisição: ', error);
     return throwError(error);
-  };
+  }
 }
