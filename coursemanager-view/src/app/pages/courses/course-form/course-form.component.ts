5ca863a8b5b5f43b896d18a346089071916f6268
Gustavo Avila <gustavogavila01@gmail.com>
2020-04-14 20:59:25 -0300

    Form de cadastro de cursos

--- coursemanager-view/src/app/pages/courses/course-form/course-form.component.ts
+++ coursemanager-view/src/app/pages/courses/course-form/course-form.component.ts
@@ -1,12 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injector } from '@angular/core';
+import { BaseResourceFormComponent } from 'src/app/shared/components/base-resource-form/base-resource-form.component';
+import { Course } from '../shared/course.model';
+import { CourseService } from '../shared/course.service';
+import { Validators } from '@angular/forms';
+import { TeacherService } from '../../teachers/shared/teacher.service';
+import { Teacher } from '../../teachers/shared/teacher.model';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
   styleUrls: ['./course-form.component.css'],
 })
-export class CourseFormComponent implements OnInit {
-  constructor() {}
+export class CourseFormComponent extends BaseResourceFormComponent<Course> implements OnInit {
 
-  ngOnInit(): void {}
+  teachers: Teacher[] = [];
+
+  constructor(
+    protected injector: Injector,
+    protected courseService: CourseService,
+    private teacherService: TeacherService,
+    ) {
+      super(injector, new Course(), courseService, Course.fromJson);
+    }
+
+  ngOnInit() {
+    super.ngOnInit();
+    this.teacherService.findAll().subscribe(
+      data => this.teachers = data,
+      error => console.log(error),
+    );
+  }
+
+  protected buildResourceForm(): void {
+    this.resourceForm = this.formBuilder.group({
+      id: [null],
+      name: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(200),
+        ],
+      ],
+      description: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(200),
+        ]
+      ],
+      price: [
+        null,
+        [
+          Validators.required,
+        ]
+      ],
+      teacher: [
+        null,
+        [
+          Validators.required,
+        ]
+      ],
+    });
+  }
+
+  protected creationPageTitle = (): string => 'Cadastro de Novo Curso';
+
+  protected editionPageTitle = (): string =>
+    this.resource?.name ? `Editando Curso: ${this.resource?.name}` : '';
 }
--- coursemanager-view/src/app/shared/components/base-resource-form/base-resource-form.component.ts
+++ coursemanager-view/src/app/shared/components/base-resource-form/base-resource-form.component.ts
@@ -128,4 +128,8 @@ export abstract class BaseResourceFormComponent<T extends BaseResourceModel>
       ];
     }
   };
+
+  public compareEntities(obj1: any, obj2: any) {
+    return obj1 && obj2 ? obj1.id === obj2.id : obj1 === obj2;
+  }
 }
